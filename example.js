b0f336b5b9c6ee11ee8885d6473762d2d8faab8c
Jannis R <mail@jannisr.de>
2017-05-31 17:09:10 +0200

    store in hyperdrive

---
+++ example.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path')
+
+const createFeed = require('.')
+
+const dest = path.join(__dirname, 'example.wiki.hyperdrive')
+
+const onProgress = (change) => {
+	console.info(change.type, change.pageTitle)
+}
+
+const onError = (err, change) => {
+	console.error(change.type, change.pageTitle, err.message)
+	process.exitCode = 1
+}
+
+createFeed(dest, onProgress, onError)
+// todo
--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 'use strict'
 
-// todo
+const hyperdrive = require('hyperdrive')
+
+const articlesFeed = require('./articles-feed')
+
+const createFeed = (dir, onProgress, onError) => {
+	const archive = hyperdrive(dir)
+
+	articlesFeed()
+	.on('error', onError)
+	.on('data', (change) => {
+		archive.writeFile(change.pageSlug + '.wikitext', change.newContent, (err) => {
+			if (err) onError(err, change)
+			else onProgress(change)
+		})
+	})
+
+	return archive
+}
+
+module.exports = createFeed
