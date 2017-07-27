868f03bf02dd737a5f5326a8cc5ea2f3f348aefa
Jannis R <mail@jannisr.de>
2017-07-27 18:53:13 +0200

    remove CLI stuff

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 'use strict'
 
-const hyperdrive = require('hyperdrive')
+const pipe = require('pump')
+const readEdits = require('wikipedia-edits-stream')
+const filter = require('stream-filter')
+const through = require('through2')
 
-const articlesFeed = require('./articles-feed')
+const fetchPageRevision = require('./fetch-page-revision')
 
-const createFeed = (dir, onProgress, onError) => {
-	const archive = hyperdrive(dir)
-
-	articlesFeed()
-	.on('error', onError)
-	.on('data', (change) => {
-		archive.writeFile(change.pageSlug + '.html', change.newContent, (err) => {
-			if (err) onError(err, change)
-			else onProgress(change)
-		})
-	})
-
-	return archive
+// https://www.mediawiki.org/wiki/Manual:RCFeed#Properties
+// https://www.mediawiki.org/wiki/Manual:Recentchanges_table
+// https://www.mediawiki.org/wiki/Manual:Log_actions
+// https://meta.wikimedia.org/wiki/Help:Namespace#List_of_namespaces
+const isEnglishWikipediaChange = (c) => {
+	return c.wiki === 'enwiki'
+	&& (c.type === 'edit' || c.type === 'new')
+	&& c.namespace === 0 // todo: categories (namespace 14)
 }
 
-module.exports = createFeed
+const parseChange = (c, _, cb) => {
+	const change = {
+		// todo: id?, bot, user, timestamp, minor
+		type: c.type,
+		pageTitle: c.title,
+		pageSlug: c.meta.uri.split('wiki/')[1], // todo: make this robust
+		pageURL: c.meta.uri,
+		oldRevision: c.revision.old,
+		newRevision: c.revision.new,
+		server: c.server_url,
+		comment: c.comment
+	}
+
+	fetchPageRevision(change.pageSlug, change.newRevision)
+	.then((content) => {
+		change.newContent = content
+		cb(null, change)
+	})
+	.catch(cb)
+}
+
+const articlesFeed = () => {
+	const out = through.obj(parseChange)
+
+	pipe(
+		readEdits(),
+		filter.obj(isEnglishWikipediaChange),
+		out,
+		(err) => out.emit('error', err)
+	)
+
+	return out
+}
+
+module.exports = articlesFeed
