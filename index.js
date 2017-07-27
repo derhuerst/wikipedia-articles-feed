27f806fcadc44877461ecc2d7d3c78ba7fc5adaa
Jannis R <mail@jannisr.de>
2017-07-28 00:49:56 +0200

    refactor code

--- index.js
+++ index.js
@@ -2,47 +2,18 @@
 
 const pipe = require('pump')
 const readEdits = require('wikipedia-edits-stream')
-const filter = require('stream-filter')
-const through = require('through2')
-const fetchPageRevision = require('fetch-wikipedia-page-revision')
 
-// https://www.mediawiki.org/wiki/Manual:RCFeed#Properties
-// https://www.mediawiki.org/wiki/Manual:Recentchanges_table
-// https://www.mediawiki.org/wiki/Manual:Log_actions
-// https://meta.wikimedia.org/wiki/Help:Namespace#List_of_namespaces
-const isEnglishWikipediaChange = (c) => {
-	return c.wiki === 'enwiki'
-	&& (c.type === 'edit' || c.type === 'new')
-	&& c.namespace === 0 // todo: categories (namespace 14)
-}
-
-const parseChange = (c, _, cb) => {
-	const change = {
-		// todo: id?, bot, user, timestamp, minor
-		type: c.type,
-		pageTitle: c.title,
-		pageSlug: c.meta.uri.split('wiki/')[1], // todo: make this robust
-		pageURL: c.meta.uri,
-		oldRevision: c.revision.old,
-		newRevision: c.revision.new,
-		server: c.server_url,
-		comment: c.comment
-	}
-
-	fetchPageRevision(change.pageSlug, change.newRevision)
-	.then((content) => {
-		change.newContent = content
-		cb(null, change)
-	})
-	.catch(cb)
-}
+const isEnglishArticleChange = require('./lib/is-english-article-change')
+const parseChanges = require('./lib/parse-changes')
+const fetchRevisions = require('./lib/fetch-revisions')
 
 const articlesFeed = () => {
-	const out = through.obj(parseChange)
+	const out = fetchRevisions()
 
 	pipe(
 		readEdits(),
-		filter.obj(isEnglishWikipediaChange),
+		isEnglishArticleChange(),
+		parseChanges(),
 		out,
 		(err) => out.emit('error', err)
 	)
